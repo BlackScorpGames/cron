577b66ab3158742ebb711be4e3c08603629081bf
BlackScorp <witalimik@web.de>
2012-01-04 23:21:16 +0100

    Basic Scene and First ship added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+window.onload = function(){
+    Crafty.init();
+    Crafty.canvas.init();
+    Crafty.sprite(60,50,"assets/img/Ships_1.png",{
+        ship1 : [0,0],
+        ship2 : [1,0],
+        shadow_ship1: [0,1],
+        shadow_ship2: [1,1]
+    });
+    Crafty.scene("game",function(){
+        Crafty.background("url(assets/img/dunes_simple.jpg)");
+        Crafty.c("Player",{});
+        console.log(Crafty.viewport);
+        Crafty.e("2D,Canvas,ship1").attr({x:Crafty.viewport.width/2,y:Crafty.viewport.height});
+    });
+    Crafty.scene("game");
+}
