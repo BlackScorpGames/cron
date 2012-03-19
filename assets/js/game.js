30c3461580f2c1a96be0effd51abef1643ad41cc
BlackScorp <witalimik@web.de>
2012-03-19 16:50:58 +0100

    added possiblity to install cron as mozilla app

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,11 +2,12 @@
  * This is the Main JS File
  */
 $(function(){
+
     //Init Crafty
     Crafty.init(600,500);
     //Add Canvas Element
     Crafty.canvas.init();
-   //Set canvas under interface
+    //Set canvas under interface
     Crafty.canvas._canvas.style.zIndex = '1';
     
     //play the loading scene
